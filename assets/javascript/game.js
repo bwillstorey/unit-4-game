ca334332f1a7616d37b002ac22a245c8d9e95f06
bwillstorey <bwillstorey@gmail.com>
2019-02-16 19:03:27 -0600

    add display update function, img styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,27 @@
 $(document).ready(function() {
 
+// Define global variables
+
     var randomNumber = Math.floor(Math.random() * 120) +19
     var wins
     var losses
     var currentScore = 0;
 
-console.log(randomNumber);
+// FUNCTIONS
+
+
+// Call Functions
+
+
+// Assign values to IDs
+
+function displayUpdate () {
+    $("#random.number").text(randomNumber);
+    $("#wins").text(wins);
+    $("#losses").text(losses);
+    $("#points").text(currentScore)
+
+}
 
 
 })
