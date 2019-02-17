d77cdeeadfb07180bdc79420c145227e6837d975
bwillstorey <bwillstorey@gmail.com>
2019-02-16 18:49:35 -0600

    linking jquery and random variable assigned

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+    var randomNumber = Math.floor(Math.random() * 120) +19
+    var wins
+    var losses
+    var currentScore = 0;
+
+console.log(randomNumber);
+
+
+})
